aa5f88af78f68e9d764d1efea67c70bfcbba981d
abdullah Al Omar <abdullahalomar048@gmail.com>
2024-02-17 01:32:47 +0600

    login authentication done

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { selectCurrentUser } from "../../redux/features/auth/authSlice";
+import { useAppSelector } from "../../redux/features/hooks";
 import { adminPaths } from "../../routes/admin.routes";
 import { facultyPaths } from "../../routes/faculty.routes";
 import { studentPaths } from "../../routes/student.routes";
@@ -12,10 +14,10 @@ const userRole = {
 };
 
 const Sidebar = () => {
-  const role = "admin";
+  const user = useAppSelector(selectCurrentUser);
   let sidebarITems;
 
-  switch (role) {
+  switch (user!.role) {
     case userRole.ADMIN:
       sidebarITems = sidebarItemsGenerator(adminPaths, userRole.ADMIN);
       break;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,46 +1,59 @@
-import { Button } from "antd";
-import { useForm } from "react-hook-form";
+import { Button, Row } from "antd";
+import { FieldValues } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/features/hooks";
-import { setUser } from "../redux/features/auth/authSlice";
+import { TUser, setUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import PhForm from "../components/form/PhForm";
+import PhInput from "../components/form/PhInput";
 
 const Login = () => {
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const { register, handleSubmit } = useForm({
-    defaultValues: {
-      id: "A-0001",
-      password: "admin123",
-    },
-  });
+  // const { register, handleSubmit } = useForm({
+  //   defaultValues: {
+  //     id: "A-0001",
+  //     password: "admin123",
+  //   },
+  // });
 
-  const [login, { error }] = useLoginMutation();
+  const defaultValues = {
+    id: "A-0001",
+    password: "admin123",
+  };
 
-  const onSubmit = async (data) => {
-    const userInfo = {
-      id: data.id,
-      password: data.password,
-    };
-    const res = await login(userInfo).unwrap();
+  const [login] = useLoginMutation();
 
-    const user = verifyToken(res.data.accessToken);
+  const onSubmit = async (data: FieldValues) => {
+    console.log(data);
 
-    dispatch(setUser({ user: user, token: res.data.accessToken }));
+    const toastId = toast.loading("Logging in");
+    try {
+      const userInfo = {
+        id: data.id,
+        password: data.password,
+      };
+      const res = await login(userInfo).unwrap();
+
+      const user = verifyToken(res.data.accessToken) as TUser;
+
+      dispatch(setUser({ user: user, token: res.data.accessToken }));
+      toast.success("Logged in", { id: toastId, duration: 2000 });
+      navigate(`/${user.role}/dashboard`);
+    } catch (error) {
+      toast.error("Something went wrong", { id: toastId, duration: 2000 });
+    }
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="id">ID: </label>
-          <input type="text" id="id" {...register("id")} />
-        </div>
-        <div>
-          <label htmlFor="password">Password: </label>
-          <input type="password" id="password" {...register("password")} />
-        </div>
+    <Row justify="center" align="middle" style={{ height: "100vh" }}>
+      <PhForm onSubmit={onSubmit} defaultValues={defaultValues}>
+        <PhInput type="text" name="id" label="ID:" />
+        <PhInput type="text" name="password" label="Password:" />
         <Button htmlType="submit">Login</Button>
-      </form>
-    </div>
+      </PhForm>
+    </Row>
   );
 };
 
